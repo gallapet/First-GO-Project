07168ebb788a6f3f592478a73a89ff453f8f1dad
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2022-01-24 16:46:01 +0000

    starting webservice

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"github.com/pluralsight/webservice/models"
+	"net/http"
+
+	"github.com/pluralsight/webservice/controllers"
 )
 
 func main() {
-	u := models.User{
-		ID: 2,
-		FirstName: "Peter",
-		LastName: "Gallacher",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
